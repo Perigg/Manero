480245c2b776e90ef7b416cf36f0a79cfda197f7
Perigg <per.igg83@gmail.com>
2024-05-23 15:56:18 +0200

    added order, page, productservice, orderitem model

---
+++ Manero/Manero/Components/Models/OrderItem.cs
@@ -0,0 +1,14 @@
+﻿namespace Manero.Components.Models
+{
+    public class OrderItem
+    {
+        public int OrderItemId { get; set; }
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+        public string? Color { get; set; }
+        public string? Size { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ Manero/Manero/Services/ProductService.cs
@@ -0,0 +1,34 @@
+﻿using Manero.Components.Models;
+
+namespace Manero.Services
+{
+    public class ProductService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ProductService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        
+        // Methods for Order
+        public async Task<List<OrderItem>> GetOrderItemsAsync(int orderId)
+        {
+            var apiUrl = $"orders/{orderId}/items";
+            return await _httpClient.GetFromJsonAsync<List<OrderItem>>(apiUrl) ?? new List<OrderItem>();
+        }
+
+        public async Task UpdateOrderItemAsync(OrderItem item)
+        {
+            var apiUrl = $"orders/items/{item.OrderItemId}";
+            await _httpClient.PutAsJsonAsync(apiUrl, item);
+        }
+
+        public async Task RemoveOrderItemAsync(int orderItemId)
+        {
+            var apiUrl = $"orders/items/{orderItemId}";
+            await _httpClient.DeleteAsync(apiUrl);
+        }
+    }
+}
