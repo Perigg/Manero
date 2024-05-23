dde39c347f1686d758b57f235c696225f31883e4
Perigg <per.igg83@gmail.com>
2024-05-23 16:07:30 +0200

    fix injection, productService, registers

---
+++ Manero/Manero/Services/ProductService.cs
@@ -0,0 +1,21 @@
+﻿using Manero.Components.Models;
+
+namespace Manero.Services
+{
+    public class ProductService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ProductService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        // Methods for Cart
+        public async Task<List<CartItem>> GetCartItemsAsync()
+        {
+            var apiUrl = "api/GetCartItems?code=zgpZKFJ5E4w3k2G9xYQLZUPC6HrvvbSjReuYJrudkTkwAzFu5t6u8g==";
+            return await _httpClient.GetFromJsonAsync<List<CartItem>>(apiUrl) ?? new List<CartItem>();
+        }
+    }
+}
