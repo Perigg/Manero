e82abb64530546ff045a0903190ae7b7735af88d
Perigg <per.igg83@gmail.com>
2024-05-23 15:26:11 +0200

    changed database variable to match productcatalog

--- Manero/Manero/Components/Models/CartItem.cs
+++ Manero/Manero/Components/Models/CartItem.cs
@@ -4,7 +4,7 @@ public class CartItem
 {
     public int CartItemID { get; set; }
     public int ProductID { get; set; }
-    public string? Title { get; set; }
+    public string? Name { get; set; }
     public decimal Price { get; set; }
     public string? Color { get; set; }
     public string? Size { get; set; }
