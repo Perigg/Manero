a4d0d614f48730a581f42c9027dcfb18a8eb707e
Perigg <per.igg83@gmail.com>
2024-05-23 11:23:34 +0200

    moved the cart page to Manero main repo

--- Manero/Manero/Registrations/ServiceRegistrations.cs
+++ Manero/Manero/Registrations/ServiceRegistrations.cs
@@ -4,6 +4,10 @@
     {
         public static void AddServiceRegistrations(this IServiceCollection services, IConfiguration config) {
 
+            //services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://cart-product-provider.azurewebsites.net/") });
+
+            services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://cart-product-provider.azurewebsites.net/api/GetCartItems?code=zgpZKFJ5E4w3k2G9xYQLZUPC6HrvvbSjReuYJrudkTkwAzFu5t6u8g==") });
+
         }
     }
 }
